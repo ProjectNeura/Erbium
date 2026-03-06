aa7d1b64d2beeb7d1a366ed7ee7821494e0899b2
ATATC <futerry@outlook.com>
2026-03-06 10:52:19 -0500

    Routine updates.

--- erbium/server/app.py
+++ erbium/server/app.py
@@ -1,11 +1,13 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from os.path import abspath
 from typing import Any
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
-from erbium.api import Scheduler
+from erbium.api import Scheduler, Job
 
 
 @dataclass
@@ -17,9 +19,6 @@ class Runtime(object):
         if self.scheduler:
             return self.scheduler
         raise RuntimeError("Scheduler not initialized")
-
-    def running_containers(self) -> dict[str, Any]:
-        return self.get_scheduler().running_containers
 
 
 app: FastAPI = FastAPI(title="LEADS VeC Remote Analyst")
@@ -37,11 +36,21 @@ with open(f"{abspath(__file__)[:-6]}assets/index.html") as f:
     runtime.homepage = f.read()
 
 
-@app.get("/")
-def index() -> str:
+@app.get("/", response_class=HTMLResponse)
+async def index() -> str:
     return runtime.homepage
 
 
 @app.get("/running_containers")
-def get_running_containers() -> dict[str, Any]:
-    return runtime.running_containers()
+async def get_running_containers() -> dict[str, Any]:
+    return {k: asdict(v) for k, v in runtime.get_scheduler().running_containers.items()}
+
+
+class JobModel(BaseModel):
+    name: str
+    requested_gpus: set[int]
+    requested_run_time_hrs: float
+
+@app.post("/schedule_job")
+async def schedule_job(job: JobModel) -> dict[str, Any]:
+    return asdict(runtime.get_scheduler().schedule(Job(job.name, job.requested_gpus, job.requested_run_time_hrs)))
