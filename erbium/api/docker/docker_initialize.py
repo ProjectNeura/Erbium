8fece1354c38bfcff2321ef7333611495f2f259f
ATATC <futerry@outlook.com>
2026-03-23 14:17:42 -0400

    Routine updates.

--- erbium/api/docker/docker_initialize.py
+++ erbium/api/docker/docker_initialize.py
@@ -1,2 +1,2 @@
 def command_to_initialize_docker(cloudflared_tunnel_token: str, *, shared_network: str = "labnet") -> str:
-    return f"docker network inspect {shared_network} >/dev/null 2>&1 || docker network create {shared_network} && docker run -d --name cloudflared --network {shared_network} cloudflare/cloudflared:latest tunnel run --token {cloudflared_tunnel_token}"
+    return f"(docker network inspect {shared_network} >/dev/null 2>&1 || docker network create {shared_network}) && docker run -d --name cloudflared --network {shared_network} cloudflare/cloudflared:latest tunnel run --token {cloudflared_tunnel_token}"
